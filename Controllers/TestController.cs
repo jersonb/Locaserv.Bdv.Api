cf472251233ea9da2674db26b94c1aea5134dd95
jersonb <jersonlbrito@hotmail.com>
2022-10-08 09:30:46 -0300

    feat: Inicia

---
+++ Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Locaserv.Bdv.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+            => Ok("Deu bom!");
+    }
+}
